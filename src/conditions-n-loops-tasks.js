7adc266ca307bcc6247bc7494261ff0f32ab2a28
JulietBrn <shishkina.ja@gmail.com>
2024-01-21 17:09:29 +0100

    feat: add some solution

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -120,8 +120,121 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  let res = '';
+  if (num <= 10) {
+    switch (num) {
+      case 1: {
+        res += 'I';
+        break;
+      }
+      case 2: {
+        res += 'II';
+        break;
+      }
+      case 3: {
+        res += 'III';
+        break;
+      }
+      case 4: {
+        res += 'IV';
+        break;
+      }
+      case 5: {
+        res += 'V';
+        break;
+      }
+      case 6: {
+        res += 'VI';
+        break;
+      }
+      case 7: {
+        res += 'VII';
+        break;
+      }
+      case 8: {
+        res += 'VIII';
+        break;
+      }
+      case 9: {
+        res += 'IX';
+        break;
+      }
+      case 10: {
+        res += 'X';
+        break;
+      }
+      default: {
+        return '';
+      }
+    }
+  } else {
+    const des = Math.trunc(num / 10);
+    switch (des) {
+      case 1: {
+        res += 'X';
+        break;
+      }
+      case 2: {
+        res += 'XX';
+        break;
+      }
+      case 3: {
+        res += 'XXX';
+        break;
+      }
+      default: {
+        return '';
+      }
+    }
+    const n = num % (des * 10);
+    switch (n) {
+      case 1: {
+        res += 'I';
+        break;
+      }
+      case 2: {
+        res += 'II';
+        break;
+      }
+      case 3: {
+        res += 'III';
+        break;
+      }
+      case 4: {
+        res += 'IV';
+        break;
+      }
+      case 5: {
+        res += 'V';
+        break;
+      }
+      case 6: {
+        res += 'VI';
+        break;
+      }
+      case 7: {
+        res += 'VII';
+        break;
+      }
+      case 8: {
+        res += 'VIII';
+        break;
+      }
+      case 9: {
+        res += 'IX';
+        break;
+      }
+      case 10: {
+        res += 'X';
+        break;
+      }
+      default: {
+        return '';
+      }
+    }
+  }
+  return res;
 }
 
 /**
